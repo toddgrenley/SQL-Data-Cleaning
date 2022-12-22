ff0b615302c4a8cd4de233b5f80278eb14187315
toddgrenley <119661479+toddgrenley@users.noreply.github.com>
2022-12-22 08:42:12 -0800

    Add files via upload

---
+++ NashvilleHousingDataCleaning.sql
@@ -0,0 +1,120 @@
+/*
+
+Cleaning Data with SQL
+
+*/
+
+
+SELECT *
+FROM [Portfolio Project]..NashvilleHousing
+
+---------------------------------------------------------------------------------------------------------------------
+
+-- Standardize Date Format
+
+SELECT SaleDateConverted, CONVERT(Date, SaleDate)
+FROM [Portfolio Project]..NashvilleHousing
+
+UPDATE [Portfolio Project]..NashvilleHousing
+SET SaleDate = CONVERT(Date, SaleDate)
+
+ALTER TABLE [Portfolio Project]..NashvilleHousing
+ADD SaleDateConverted Date;
+
+UPDATE NashvilleHousing
+SET SaleDateConverted = CONVERT(Date, SaleDate)
+
+
+---------------------------------------------------------------------------------------------------------------------
+
+-- Populate Property Address Data
+
+SELECT *
+FROM [Portfolio Project]..NashvilleHousing
+--WHERE PropertyAddress is null
+ORDER BY ParcelID
+
+SELECT a.ParcelID, a.PropertyAddress, b.ParcelID, b.PropertyAddress, ISNULL (a.PropertyAddress, b.PropertyAddress)
+FROM [Portfolio Project]..NashvilleHousing AS a
+JOIN [Portfolio Project]..NashvilleHousing AS b
+	ON a.ParcelID = b.ParcelID
+	AND a.[UniqueID ] <> b.[UniqueID ]
+WHERE a.PropertyAddress is null
+
+UPDATE a
+SET PropertyAddress = ISNULL (a.PropertyAddress, b.PropertyAddress)
+FROM [Portfolio Project]..NashvilleHousing AS a
+JOIN [Portfolio Project]..NashvilleHousing AS b
+	ON a.ParcelID = b.ParcelID
+	AND a.[UniqueID ] <> b.[UniqueID ]
+WHERE a.PropertyAddress is null
+
+
+---------------------------------------------------------------------------------------------------------------------
+
+-- Breaking Address into Individual Columns
+
+SELECT PropertyAddress
+FROM [Portfolio Project]..NashvilleHousing
+--WHERE PropertyAddress is null
+--ORDER BY ParcelID
+
+SELECT
+SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) -1 ) AS Address
+, SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) +1 , LEN(PropertyAddress)) AS Address
+FROM [Portfolio Project]..NashvilleHousing
+
+
+ALTER TABLE [Portfolio Project]..NashvilleHousing
+ADD PropertySplitAddress Nvarchar(255);
+
+UPDATE NashvilleHousing
+SET PropertySplitAddress = SUBSTRING(PropertyAddress, 1, CHARINDEX(',', PropertyAddress) -1 )
+
+
+ALTER TABLE [Portfolio Project]..NashvilleHousing
+ADD PropertySplitCity Nvarchar(255);
+
+UPDATE NashvilleHousing
+SET PropertySplitCity = SUBSTRING(PropertyAddress, CHARINDEX(',', PropertyAddress) +1 , LEN(PropertyAddress))
+
+
+SELECT *
+FROM NashvilleHousing
+
+
+SELECT OwnerAddress
+FROM NashvilleHousing
+
+
+SELECT
+PARSENAME(REPLACE(OwnerAddress, ',', '.'), 3)
+,PARSENAME(REPLACE(OwnerAddress, ',', '.'), 2)
+,PARSENAME(REPLACE(OwnerAddress, ',', '.'), 1)
+FROM [Portfolio Project]..NashvilleHousing
+
+
+ALTER TABLE NashvilleHousing
+ADD OwnerSplitAddress Nvarchar(255);
+
+UPDATE NashvilleHousing
+SET OwnerSplitAddress = PARSENAME(REPLACE(OwnerAddress, ',', '.'), 3)
+
+
+ALTER TABLE NashvilleHousing
+ADD OwnerSplitCity Nvarchar(255);
+
+UPDATE NashvilleHousing
+SET OwnerSplitCity = PARSENAME(REPLACE(OwnerAddress, ',', '.'), 2)
+
+
+ALTER TABLE NashvilleHousing
+ADD OwnerSplitState Nvarchar(255);
+
+UPDATE NashvilleHousing
+SET OwnerSplitState = PARSENAME(REPLACE(OwnerAddress, ',', '.'), 1)
+
+
+
+SELECT *
+FROM NashvilleHousing
